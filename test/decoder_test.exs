6b93e08673b60825ffaff1d76e27acc0b794ed70
meh <meh@schizofreni.co>
2013-07-19 06:44:18 +0200

    test: add a bunch of tests

---
+++ test/decoder_test.exs
@@ -0,0 +1,48 @@
+Code.require_file "test_helper.exs", __DIR__
+
+defmodule DecoderTest do
+  use ExUnit.Case
+
+  defrecord Foo, [:a, :b]
+  defrecord Bar, [:a, :b]
+
+  defimpl JSON.Encoder, for: Bar do
+    def to_json(Bar[a: a, b: b], _) do
+      [data: [a, b]]
+    end
+  end
+
+  defimpl JSON.Decoder, for: Bar do
+    def from_json({ _, parsed, _ }) do
+      [a, b] = parsed["data"]
+
+      Bar[a: a, b: b]
+    end
+  end
+
+  test "decodes numbers correctly" do
+    assert JSON.decode!("4")      == 4
+    assert JSON.decode!("2.3")    == 2.3
+    assert JSON.decode!("2.4583") == 2.4583
+  end
+
+  test "decodes strings correctly" do
+    assert JSON.decode!(%B/"lol"/)    == "lol"
+    assert JSON.decode!(%B/"\\\r\n"/) == "\\\r\n"
+  end
+
+  test "decodes objects correctly" do
+    assert JSON.decode!(%B/{"lol":"wut"}/, keys: :atoms)         == [lol: "wut"]
+    assert JSON.decode!(%B/{"lol":{"omg":"wut"}}/, keys: :atoms) == [lol: [omg: "wut"]]
+  end
+
+  test "decodes arrays correctly" do
+    assert JSON.decode!(%B/[1,2,3]/)                       == [1, 2, 3]
+    assert JSON.decode!(%B/[{"lol":"wut"},{"omg":"wut"}]/, keys: :atoms) == [[lol: "wut"], [omg: "wut"]]
+  end
+
+  test "decodes records correctly" do
+    assert JSON.decode!(%B/{"a":2,"b":3}/, as: Foo)  == Foo[a: 2, b: 3]
+    assert JSON.decode!(%B/{"data":[2,3]}/, as: Bar) == Bar[a: 2, b: 3]
+  end
+end
---
+++ test/encoder_test.exs
@@ -0,0 +1,40 @@
+Code.require_file "test_helper.exs", __DIR__
+
+defmodule EncoderTest do
+  use ExUnit.Case
+
+  defrecord Foo, [:a, :b]
+  defrecord Bar, [:a, :b]
+
+  defimpl JSON.Encoder, for: Bar do
+    def to_json(Bar[a: a, b: b], _) do
+      [data: [a, b]]
+    end
+  end
+
+  test "encodes numbers correctly" do
+    assert JSON.encode(4)      == "4"
+    assert JSON.encode(2.3)    == "2.3"
+    assert JSON.encode(2.4583) == "2.4583"
+  end
+
+  test "encodes strings correctly" do
+    assert JSON.encode("lol")    == %B/"lol"/
+    assert JSON.encode("\\\r\n") == %B/"\\\r\n"/
+  end
+
+  test "encodes objects correctly" do
+    assert JSON.encode([lol: "wut"])        == %B/{"lol":"wut"}/
+    assert JSON.encode([lol: [omg: "wut"]]) == %B/{"lol":{"omg":"wut"}}/
+  end
+
+  test "encodes arrays correctly" do
+    assert JSON.encode([1, 2, 3])                    == %B/[1,2,3]/
+    assert JSON.encode([[lol: "wut"], [omg: "wut"]]) == %B/[{"lol":"wut"},{"omg":"wut"}]/
+  end
+
+  test "encodes records correctly" do
+    assert JSON.encode(Foo[a: 2, b: 3]) == %B/{"a":2,"b":3}/
+    assert JSON.encode(Bar[a: 2, b: 3]) == %B/{"data":[2,3]}/
+  end
+end
