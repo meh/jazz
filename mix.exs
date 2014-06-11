46079b7571d0c70874a0ca002612645d0bafd64c
meh <meh@schizofreni.co>
2014-06-11 21:55:52 +0200

    mix: bump version

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Jazz.Mixfile do
 
   def project do
     [ app: :jazz,
-      version: "0.1.0",
-      elixir: "~> 0.13.0",
+      version: "0.1.1",
+      elixir: "~> 0.14.0",
       package: package,
       description: "JSON handling library for Elixir." ]
   end
